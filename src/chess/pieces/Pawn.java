6928f8882c3ae15cfc0b7bb147e2c4b8b60fb5f5
FelipeKonig <49540283+FelipeKonig@users.noreply.github.com>
2021-06-07 17:23:32 -0300

    Fix pawn movement

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -34,7 +34,7 @@ public class Pawn extends ChessPiece {
 			Position p2 = new Position(position.getRow() - 1, position.getColumn());
 
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
-					&& !getBoard().thereIsAPiece(p2)) {
+					&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 
@@ -76,7 +76,7 @@ public class Pawn extends ChessPiece {
 			Position p2 = new Position(position.getRow() + 1, position.getColumn());
 
 			if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2)
-					&& !getBoard().thereIsAPiece(p2)) {
+					&& !getBoard().thereIsAPiece(p2) && getMoveCount() == 0) {
 				mat[p.getRow()][p.getColumn()] = true;
 			}
 
